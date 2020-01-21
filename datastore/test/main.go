5476bb9cca77797f6ffa4f0eb8b9680ad6596c34
A WEI <wei.141227@gmail.com>
2020-01-22 00:14:37 +0800

    test4

--- datastore/test/main.go
+++ datastore/test/main.go
@@ -13,4 +13,4 @@ func main() {
 }
 
 //部署指令
-//gcloud functions deploy PutParkingInfo --source https://source.developers.google.com/projects/parkingproject-261207/repos/github_wei02427_linebotproject/moveable-aliases/master/paths/ --runtime=go113 --trigger-topic=updateInfo
+//gcloud functions deploy PutParkingInfo --source https://source.developers.google.com/projects/parkingproject-261207/repos/github_wei02427_linebotproject/moveable-aliases/master/paths/src --runtime=go113 --trigger-topic=updateInfo
