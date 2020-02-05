3e48b47e2a6eabba331c9309cb95a2afceb7525b
Awei <wei.141227@gmail.com>
2020-02-05 11:30:31 +0800

    test5

--- fulfillment/function.go
+++ fulfillment/function.go
@@ -60,11 +60,15 @@ type response struct {
 	FulfillmentText string `json:"fulfillmentText"`
 }
 
-//Fulfillment 查詢車位
-func Fulfillment(w http.ResponseWriter, r *http.Request) {
+func init() {
 	bot, err = linebot.New("57cc60c3fc1530cc32ba896e1c4b7856", "GiKIwKk+Lwku0WeGEGnlEDBDDGC67tQVCSIMbcQaKpA2IyZPU6OgVSIdI0h1HUUG2Ky/psNLEEkjfnEZGITnJolxlEScGgLoWT/iKpwyinf/IJDgeB5gnIB0zmuag0vYlcs7WgOYdUg0CwbGXlWKIwdB04t89/1O/w1cDnyilFU=")
 	dp.init("parkingproject-261207" /*, "parkingproject-261207-2933e4112308.json"*/, "zh-TW", "Asia/Hong_Kong")
-	events, err := bot.ParseRequest(r)
+}
+
+//Fulfillment 查詢車位
+func Fulfillment(w http.ResponseWriter, r *http.Request) {
+
+	events, err = bot.ParseRequest(r)
 
 	if err != nil {
 		if err == linebot.ErrInvalidSignature {
